4ea59f186ec265a2ef9c6174488fff6ed018c949
你的微笑暖暖的 <1245488569@qq.com>
2024-02-19 21:39:22 +0800

    feat: 完成redux的集成

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { HashRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
