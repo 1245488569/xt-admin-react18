7dc4243ff62971004991254929119404ae683e93
你的微笑暖暖的 <1245488569@qq.com>
2024-03-05 21:48:14 +0800

    feat: 完成顶部导航的配色处理

--- src/layouts/components/top/index.tsx
+++ src/layouts/components/top/index.tsx
@@ -3,7 +3,7 @@ import type { RouteObject } from 'react-router-dom'
 import { useRouteLoaderData } from 'react-router-dom'
 import type { MenuProps } from 'antd'
 import Logo from '../logo'
-import { TopNavWrapper } from './style'
+import { GlobalStyles, TopNavWrapper } from './style'
 import { useSysConfigStore } from '@/stores/config'
 import type { ILayoutLoader } from '@/types/common'
 import { useMenuStore } from '@/stores/menu'
@@ -11,9 +11,8 @@ import { getCatchRouteMeta } from '@/utils/router'
 import { rootRoutes } from '@/router'
 
 export default function Top() {
-  const { layoutMode, colorScheme, mainMenuBgColor, menuBgColor, mainMenuTextColor, menuTextColor, mainMenuHoverBgColor, menuHoverBgColor, mainMenuHoverTextColor, menuHoverTextColor, mainMenuActiveBgColor, menuActiveBgColor, mainMenuActiveTextColor, menuActiveTextColor } = useSysConfigStore(useShallow(state => ({
+  const { layoutMode, mainMenuBgColor, menuBgColor, mainMenuTextColor, menuTextColor, mainMenuHoverBgColor, menuHoverBgColor, mainMenuHoverTextColor, menuHoverTextColor, mainMenuActiveBgColor, menuActiveBgColor, mainMenuActiveTextColor, menuActiveTextColor } = useSysConfigStore(useShallow(state => ({
     layoutMode: state.app.layoutMode,
-    colorScheme: state.colorScheme,
     mainMenuBgColor: state.theme.mainMenuBgColor,
     menuBgColor: state.theme.menuBgColor,
     mainMenuTextColor: state.theme.mainMenuTextColor,
@@ -67,6 +66,7 @@ export default function Top() {
           path: item.path,
           icon: item.meta?.icon && <SvgIcon name={item.meta?.icon} />,
           children: filteredChildren?.length ? filteredChildren : undefined,
+          popupClassName: 'xt-admin-popup-menu',
         }
         // 过滤掉含有空children的顶层对象
         if (newItem.children?.length || newItem.path)
@@ -123,48 +123,45 @@ export default function Top() {
 
   return (
     <TopNavWrapper className="h-[var(--xt-top-nav-height)] flex flex-shrink-0 items-center px-4" $customMenuClass={menuClass}>
+      <GlobalStyles $customMenuClass={menuClass} />
       <Logo className="mr-4 text-xl" />
       <ConfigProvider theme={{
         components: {
           Menu: {
-            // TODO: 多级菜单的子菜单，选中状态的背景色不对
             activeBarHeight: 0,
             horizontalItemBorderRadius: 8,
             iconSize: 20,
             iconMarginInlineEnd: 5, // 图标与文字间距
-            itemBg: menuClass?.menuContainerBgColor, // 菜单项背景色（整个菜单背景色）
-            itemColor: menuClass?.menuTextColor, // 菜单项文字颜色（整个菜单文字色）
+            // itemBg: menuClass?.menuContainerBgColor, // 菜单项背景色（整个菜单背景色）
+            // itemColor: menuClass?.menuTextColor, // 菜单项文字颜色（整个菜单文字色）
             // horizontalItemHoverColor: '#f00', // 水平菜单项文字悬浮颜色（不清楚是啥） 对不上
 
-            horizontalItemHoverBg: colorScheme === 'dark' ? menuClass?.darkMenuHoverBgColor : menuClass?.menuHoverBgColor, // 横向菜单项横悬浮态背景色(鼠标经过)
+            // horizontalItemHoverBg: colorScheme === 'dark' ? menuClass?.darkMenuHoverBgColor : menuClass?.menuHoverBgColor, // 横向菜单项横悬浮态背景色(鼠标经过)
             // horizontalItemSelectedBg: menuClass?.menuActiveBgColor, // 水平菜单项选中态背景色(选中的项) (暗黑下没作用)
             // horizontalItemSelectedColor: menuClass?.menuActiveTextColor, // 水平菜单项文字选中颜色(只针对可跳转的菜单项) (暗黑下没作用)
 
-            itemActiveBg: colorScheme === 'dark' ? menuClass?.darkMenuActiveBgColor : menuClass?.menuActiveBgColor, // 菜单项激活态背景色(点击在popup层的菜单项，点击那瞬间的背景色)
-            itemHoverBg: menuClass?.menuHoverBgColor, // 菜单项悬浮态背景色(鼠标经过popup层的菜单项)
-            itemHoverColor: menuClass?.menuHoverTextColor, // 菜单项文字悬浮颜色(鼠标经过popup层的菜单项,文字颜色)
-            itemSelectedBg: menuClass?.menuActiveBgColor, // 菜单项选中态背景色(popup层的菜单项 选中的颜色)
-            itemSelectedColor: menuClass?.menuActiveTextColor, // 菜单项文字选中颜色(popup层的菜单项 选中的颜色，以及子菜单项选中的文字颜色)
+            // itemActiveBg: colorScheme === 'dark' ? menuClass?.darkMenuActiveBgColor : menuClass?.menuActiveBgColor, // 菜单项激活态背景色(点击在popup层的菜单项，点击那瞬间的背景色)
+            // itemHoverBg: menuClass?.menuHoverBgColor, // 菜单项悬浮态背景色(鼠标经过popup层的菜单项)
+            // itemHoverColor: menuClass?.menuHoverTextColor, // 菜单项文字悬浮颜色(鼠标经过popup层的菜单项,文字颜色)
+            // itemSelectedBg: menuClass?.menuActiveBgColor, // 菜单项选中态背景色(popup层的菜单项 选中的颜色)
+            // itemSelectedColor: menuClass?.menuActiveTextColor, // 菜单项文字选中颜色(popup层的菜单项 选中的颜色，以及子菜单项选中的文字颜色)
 
-            darkItemBg: menuClass?.darkMenuContainerBgColor, // 暗黑模式菜单项背景色
-            darkItemColor: menuClass?.darkMenuTextColor, // 暗色模式下菜单项文字颜色（整个菜单文字色）
-            darkItemHoverBg: menuClass?.darkMenuHoverBgColor,
-            darkItemSelectedBg: menuClass?.darkMenuActiveBgColor,
-            darkItemSelectedColor: menuClass?.darkMenuActiveTextColor,
-
-            subMenuItemBg: '#f00', // 子菜单项背景色(对于是popup的无效)
-            // subMenuItemBorderRadius: 20, // 子菜单项圆角
+            // darkItemBg: menuClass?.darkMenuContainerBgColor, // 暗黑模式菜单项背景色
+            // darkItemColor: menuClass?.darkMenuTextColor, // 暗色模式下菜单项文字颜色（整个菜单文字色）
+            // darkItemHoverBg: menuClass?.darkMenuHoverBgColor,
+            // darkItemSelectedBg: menuClass?.darkMenuActiveBgColor,
+            // darkItemSelectedColor: menuClass?.darkMenuActiveTextColor,
           },
         },
       }}
       >
         {/* 顶部主导航+侧边次导航 */}
         { layoutMode === 'topSubSideNav' && (
-          <Menu className="xt-menu flex-1" mode="horizontal" theme={colorScheme === 'dark' ? 'dark' : 'light'} selectedKeys={[`${mainMenuActive}`]} items={getMainMenuItems()} onClick={clickMainMenu} />
+          <Menu className="xt-menu flex-1" mode="horizontal" selectedKeys={[`${mainMenuActive}`]} items={getMainMenuItems()} onClick={clickMainMenu} />
         ) }
         {/* 只有顶部导航 */}
         { layoutMode === 'onlyTopNav' && (
-          <Menu className="xt-menu flex-1" mode="horizontal" theme={colorScheme === 'dark' ? 'dark' : 'light'} selectedKeys={[defaultActive]} items={getSubMenuItems(allSubmenu)} onClick={clickSubMenu} />
+          <Menu className="xt-menu flex-1" mode="horizontal" selectedKeys={[defaultActive]} items={getSubMenuItems(allSubmenu)} onClick={clickSubMenu} />
         ) }
       </ConfigProvider>
 
--- src/layouts/components/top/style.ts
+++ src/layouts/components/top/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 interface IMenuWrapper {
   $customMenuClass: {
@@ -24,38 +24,103 @@ export const TopNavWrapper = styled.div<IMenuWrapper>`
   }
 
   .xt-menu {
+    background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuContainerBgColor : props.$customMenuClass?.menuContainerBgColor};
+    color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuTextColor : props.$customMenuClass?.menuTextColor};
+
     .ant-menu-item, .ant-menu-submenu {
       margin-right: 8px;
-    }
+      background: transparent;
+      color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuTextColor : props.$customMenuClass?.menuTextColor};
 
-    .ant-menu-item {
       &:hover {
-        border-bottom: 2px solid ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor};
-      }
-
-      &.ant-menu-item-selected {
-        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
-        background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor};
-        border-bottom: 2px solid ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
-      }
-    }
-
-    .ant-menu-submenu {
-      &:hover {
+        background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverBgColor : props.$customMenuClass?.menuHoverBgColor};
         color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
         border-bottom: 2px solid ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor};
+
+        .ant-menu-submenu-title {
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+        }
       }
 
-      &.ant-menu-submenu-selected {
+      &.ant-menu-item-selected, &.ant-menu-submenu-selected {
         background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor};
+        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
         border-bottom: 2px solid ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+
+        .ant-menu-submenu-title {
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+        }
       }
     }
 
     .ant-menu-submenu-open {
+      background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor};
       color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
       border-bottom: 2px solid ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor};
+
+      .ant-menu-submenu-title {
+        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+      }
     }
   }
   
+`
+export const GlobalStyles = createGlobalStyle<IMenuWrapper>`
+  body {
+    .xt-admin-popup-menu {
+      .ant-menu-item {
+        background: transparent;
+        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuTextColor : props.$customMenuClass?.menuTextColor};
+
+        &:hover {
+          background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverBgColor : props.$customMenuClass?.menuHoverBgColor}!important;
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+
+          .ant-menu-submenu-title {
+            background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverBgColor : props.$customMenuClass?.menuHoverBgColor}!important;
+            color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+          }
+        }
+
+        &.ant-menu-item-selected {
+          background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor} !important;
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+
+          .ant-menu-submenu-title {
+            color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+          }
+        }
+      }
+
+      .ant-menu-submenu {
+        background: transparent;
+        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuTextColor : props.$customMenuClass?.menuTextColor};
+
+        &:hover {
+          .ant-menu-submenu-title {
+            background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverBgColor : props.$customMenuClass?.menuHoverBgColor}!important;
+            color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+          }
+        }
+
+        &.ant-menu-submenu-selected {
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+
+          .ant-menu-submenu-title {
+            background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor} !important;
+            color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveTextColor : props.$customMenuClass?.menuActiveTextColor};
+          }
+        }
+      }
+
+      .ant-menu-submenu-open {
+        color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+
+        .ant-menu-submenu-title {
+          background: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuActiveBgColor : props.$customMenuClass?.menuActiveBgColor};
+          color: ${props => props.theme.isDark ? props.$customMenuClass?.darkMenuHoverTextColor : props.$customMenuClass?.menuHoverTextColor} !important;
+        }
+      }
+    }
+  }
 `
