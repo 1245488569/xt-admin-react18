2cbff847dd0cbbe19c87b66398ed1eabe1024eba
lxc <1245488569@qq.com>
2024-03-08 15:52:12 +0800

    feat: 删除多余文件

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,22 +3,6 @@ import LayoutLoader from './LayoutLoader'
 import privateRoutes from './private'
 import Root from '@/Root'
 
-// import Layout from '@/layouts'
-
-// const Login = lazy(() => import('@/views/login'))
-// const FrameDashboard = lazy(() => import('@/views/frame_dashboard'))
-// const Demo1 = lazy(() => import('@/views/demo1'))
-// const Demo2 = lazy(() => import('@/views/demo2'))
-// const Error404 = lazy(() => import('@/views/404'))
-// const Error403 = lazy(() => import('@/views/403'))
-
-// const importRoutesData = import.meta.glob('./modules/*.tsx', { eager: true })
-
-// export const privateRoutes: RouteObject[] = []
-// Object.keys(importRoutesData).forEach((v) => {
-//   privateRoutes.push((importRoutesData[v] as any).default)
-// })
-
 export const rootRoutes: RouteObject[] = [
   {
     // lazy: () => import('@/Root'),
@@ -48,22 +32,6 @@ export const rootRoutes: RouteObject[] = [
               title: 'route.dashboard',
               icon: 'ri:home-heart-fill',
             },
-          },
-          {
-            path: '/demo1',
-            lazy: () => import('@/views/demo1'),
-            // element: <Suspense><Demo1 /></Suspense>,
-            parentIndex: 0,
-            meta: {
-              title: 'route.personal',
-              isWhite: true,
-              icon: 'ri:home-heart-fill',
-            },
-          },
-          {
-            path: '/demo2',
-            // element: <Suspense><Demo2 /></Suspense>,
-            lazy: () => import('@/views/demo2'),
           },
           ...privateRoutes,
           {
--- src/views/demo2/index.tsx
+++
@@ -1,5 +0,0 @@
-export function Component() {
-  console.log('Demo2')
-
-  return <div>Demo2</div>
-}
